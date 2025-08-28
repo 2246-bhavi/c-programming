b6c9b5c9f64bf0a30718e0bb301e81eb48c3bc9d
Bhavi Chaturvedi <bhavichaturvedi01@gmail.com>
2025-08-28 22:18:52 +0530

    Add files via upload

---
+++ q1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int num1,num2,sum;
+    printf("enter number 1:");
+    scanf("%d",&num1);
+
+    printf("enter number 2:");
+    scanf("%d",&num2);
+
+    sum=num1+num2;
+
+    printf("sum is:%d",sum);
+    return 0;
+}
