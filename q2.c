a3bfcdd795324529e93b0a7fc1f60f5357bdde4a
Bhavi Chaturvedi <bhavichaturvedi01@gmail.com>
2025-08-28 22:35:55 +0530

    Add files via upload

---
+++ q2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,sum,diff,mul,div;
+    printf("enter first number:");
+    scanf("%d",&a);
+
+    printf("enter second number:");
+    scanf("%d",&b);
+
+    sum=a+b;
+    diff=a-b;
+    mul=a*b;
+    div=a/b;
+
+    printf("sum of the numbers is:%d\n",sum);
+    printf("difference of the numbers is:%d\n",diff);
+    printf("product of the numbers is:%d\n",mul);
+    printf("division of the numbers is:%d\n",div);
+    return 0;
+}
