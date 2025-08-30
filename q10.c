51cdd6dc83bef199c25d11a86d6151ef8ebbdedd
Bhavi Chaturvedi <bhavichaturvedi01@gmail.com>
2025-08-30 23:44:45 +0530

    Add files via upload

---
+++ q10.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int h,m,s;
+    printf("enter time in seconds:");
+    scanf("%d",&s);
+
+    h=s/3600;
+    m=(s-h*3600)/60;
+    s=s-h*3600-m*60;
+
+    printf("time in mins:%d %d %d",h,m,s);
+    return 0;
+
+
+}
---
+++ q9.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    float p,r,t,si,ci;
+    printf("enter principal amount:");
+    scanf("%f",&p);
+
+    printf("enter ROI:");
+    scanf("%f",&r);
+
+    printf("enter time:");
+    scanf("%f",&t);
+
+    si=p*r*t/100;
+    ci=p*pow(1+r/100,t)-p;
+
+    printf("simple interest is:%f\n",si);
+    printf("compound interest is:%f\n",ci);
+}
