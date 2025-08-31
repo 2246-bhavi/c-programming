0ceba758e755b4456048c4ed612781b10972db52
Bhavi Chaturvedi <bhavichaturvedi01@gmail.com>
2025-08-31 23:54:39 +0530

    q19.c

--- q19.c
+++ q19.c
@@ -1,3 +1,5 @@
+//Q19: Write a program to classify a triangle as Equilateral, Isosceles, or Scalene based on its side lengths.
+
 #include<stdio.h>
 int main()
 {
@@ -18,4 +20,5 @@ int main()
         printf("is it a scalene triangle");
     }
     return 0;
+
 }
